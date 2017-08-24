e2e77e84dde09e7ef773b5ce8cc222c4093c1f36
Bogdan Vaskan <fant1k450@gmail.com>
2017-08-24 10:55:34 +0300

    почти закончил дз 1 по событиям

--- js/pictures.js
+++ js/pictures.js
@@ -2,16 +2,59 @@
 
 var galleryOverlay = document.querySelector('.gallery-overlay');
 var galleryOverlayImage = document.querySelector('.gallery-overlay-image');
+var galleryClose = document.querySelector('.gallery-overlay-close');
 var likesCount = document.querySelector('.likes-count');
 var commentsCount = document.querySelector('.comments-count');
 
-var picturesNode = document.querySelector('.pictures');
+
+var picturesContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content;
 var fragment = document.createDocumentFragment();
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
-galleryOverlay.classList.remove('hidden');
+// проверка была ли нажата кнопка enter
+var isActivateEvent = function (evt) {
+  return evt.keyCode && evt.keyCode === ENTER_KEYCODE;
+};
 
+// проверка была ли нажата кнопка escape
+var isDeactivateEvent = function (evt) {
+  return evt.keyCode && evt.keyCode === ESC_KEYCODE;
+};
+
+// функция обработчик события для enter
+var onEnterOnKeydown = function (evt) {
+  if (isDeactivateEvent(evt)) {
+    closeGallery();
+  }
+};
+
+// открываем галерею
+var openGallery = function () {
+  galleryOverlay.classList.remove('hidden');
+  document.addEventListener('onkeydown', onEnterOnKeydown);
+};
+
+// закрываем галерею
+var closeGallery = function () {
+  galleryOverlay.classList.add('hidden');
+};
+
+// вешаем обработчик события на закрытие по клику
+var onCloseGalleryClick = function () {
+  closeGallery();
+};
+
+// вешаем обработчик события на закрытие по нажатию на клавишу
+var onCloseGalleryOnKeydown = function (evt) {
+  if (isActivateEvent(evt)) {
+    closeGallery();
+  }
+};
+
+// получаем рандомный элемент массива
 var getRandomComment = function () {
   var comments = [
     'Всё отлично!',
@@ -21,14 +64,18 @@ var getRandomComment = function () {
     'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
+
   var randomElementIndex = Math.floor(Math.random() * comments.length);
+
   return comments[randomElementIndex];
 };
 
+// получаем рандомное целое число
 var getRandomInt = function (min, max) {
   return Math.round(min + Math.random() * (max - min + 1));
 };
 
+// собираем массив объектов
 var getData = function () {
   var pictures = [];
 
@@ -43,29 +90,57 @@ var getData = function () {
   return pictures;
 };
 
-var arr = getData();
-
+// рендерим картинки
 var renderImg = function (img) {
   var imageElement = pictureTemplate.cloneNode(true);
   imageElement.querySelector('img').src = img.url;
   imageElement.querySelector('.picture-likes').textContent = img.likes;
-  imageElement.querySelector('.picture-comments').textContent = img.comments;
+  imageElement.querySelector('.picture-comments').textContent = img.comments.length;
 
   return imageElement;
 };
 
+// рендерим картинку для галереи
 var renderGalleryImg = function (bigImg) {
   galleryOverlayImage.src = bigImg.url;
   likesCount.textContent = bigImg.likes;
   commentsCount.textContent = bigImg.comments;
 };
 
-var appendNodes = function () {
+// сохраняем в переменую массив объектов
+var arr = getData();
+
+// добавляем в DOM картинки
+var appendNodess = function () {
   for (var i = 0; i <= 25; i++) {
     fragment.appendChild(renderImg(arr[i]));
   }
-  picturesNode.appendChild(fragment);
+  picturesContainer.appendChild(fragment);
 };
 
-appendNodes();
+// ищем содержит ли элемент класс picture
+var activeGallery = function (evt) {
+  var currentElemnt = evt.target.classList.contains('picture') ? evt.target : evt.target.parentNode.previousElementSibling;
+  if (currentElemnt) {
+    openGallery(currentElemnt);
+  }
+};
+
+// вешаем обработчик события на картинки по клику
+var onActiveGallryClick = function (evt) {
+  activeGallery(evt);
+};
+
+// вешаем обработчик события на картинки по нажатию на enter
+var onActiveGallryOnKeydown = function (evt) {
+  if (isActivateEvent(evt)) {
+    activeGallery(evt);
+  }
+};
+
+appendNodess();
 galleryOverlay.appendChild(renderGalleryImg(arr[0]));
+picturesContainer.addEventListener('click', onActiveGallryClick);
+picturesContainer.addEventListener('onkeydown', onActiveGallryOnKeydown);
+galleryClose.addEventListener('click', onCloseGalleryClick);
+galleryClose.addEventListener('onkeydown', onCloseGalleryOnKeydown);
