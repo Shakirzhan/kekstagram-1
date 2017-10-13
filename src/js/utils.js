a6fafd7eb94a11451bdb207e1ef1b87b02a44686
Bogdan Vaskan <fant1k450@gmail.com>
2017-10-13 11:16:40 +0300

    организовал workflow и убрал закомментривоанный код

--- src/js/utils.js
+++ src/js/utils.js
@@ -12,7 +12,19 @@ window.utils = (function () {
     // проверка была ли нажата кнопка escape
     isDeactivateEvent: function (evt) {
       return evt.keyCode && evt.keyCode === ESC_KEYCODE;
+    },
+    // получаем и клонируем шаблон
+    getTemplateClone: function (template, innerSelector) {
+      var templateElem = document.querySelector(template);
+      var elemToClone;
+
+      if ('content' in templateElem) {
+        elemToClone = templateElem.content.querySelector(innerSelector);
+      } else {
+        elemToClone = templateElem.querySelector(innerSelector);
+      }
+
+      return elemToClone;
     }
   };
-
 })();
