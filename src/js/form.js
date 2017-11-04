dee21fdf7c24a4144cc97a80d2bda8ae082b678f
Bogdan Vaskan <fant1k450@gmail.com>
2017-11-04 23:14:51 +0200

    added IIFE

--- src/js/form.js
+++ src/js/form.js
@@ -1,92 +1,95 @@
 'use strict';
 
-// форма кадрирования
-var uploadOverlay = document.querySelector('.upload-overlay');
+(function () {
+  // форма кадрирования
+  var uploadOverlay = document.querySelector('.upload-overlay');
 
-var uploadImg = document.querySelector('.upload-image');
-var uploadCancel = document.querySelector('.upload-form-cancel');
-var uploadFile = document.querySelector('#upload-file');
-var commentArea = document.querySelector('.upload-form-description');
-var recizeControlFieldset = document.querySelector('.upload-resize-controls');
-var uploadHashrag = document.querySelector('.upload-form-hashtags');
-var uploadComment = document.querySelector('.upload-form-description');
-var STEP_OF_ZOOM = 25;
+  var uploadImg = document.querySelector('.upload-image');
+  var uploadCancel = document.querySelector('.upload-form-cancel');
+  var uploadFile = document.querySelector('#upload-file');
+  var commentArea = document.querySelector('.upload-form-description');
+  var recizeControlFieldset = document.querySelector('.upload-resize-controls');
+  var uploadHashrag = document.querySelector('.upload-form-hashtags');
+  var uploadComment = document.querySelector('.upload-form-description');
+  var STEP_OF_ZOOM = 25;
 
-/**
- * [скрывает форму кадрирования и показывает форму  загрузки изображения]
- */
-var showImgLoader = function () {
-  uploadFile.value = '';
+  /**
+   * [скрывает форму кадрирования и показывает форму  загрузки изображения]
+   */
+  var showImgLoader = function () {
+    uploadFile.value = '';
 
-  uploadOverlay.classList.add('hidden');
-  uploadImg.classList.remove('hidden');
-};
+    uploadOverlay.classList.add('hidden');
+    uploadImg.classList.remove('hidden');
+  };
 
-var onShowImgLoaderClick = function () {
-  showImgLoader();
-};
-
-/**
- * [Вещаем обработчик события по клавиатуре]
- * @param  {[object]} evt [передаем его в функцию показа формы загрузки]
- */
-var onShowImgLoaderOnkeydown = function (evt) {
-  if (window.utils.isActivateEvent(evt)) {
-    showImgLoader(evt);
-  }
-};
-
-/**
- * [функция обработчик события для enter]
- * @param  {[object]} evt [передаем его в функцию показа формы загрузки]
- */
-var onEnterKeydown = function (evt) {
-  if (window.utils.isDeactivateEvent(evt)) {
+  var onShowImgLoaderClick = function () {
     showImgLoader();
-  }
-};
+  };
 
-/**
- * [ показываем форму кадрирования & скрываем форму загрузки изображения]
- * [Вешаем обработчик события на эскейп для закрытия окна]
- * [отменяет закрытия окна по эскейп, когда фокус в комментарии]
- */
-var showCroppForm = function () {
-  uploadOverlay.classList.remove('hidden');
-  uploadImg.classList.add('hidden');
-
-  document.addEventListener('keydown', onEnterKeydown);
-
-  uploadComment.addEventListener('keydown', function (evt) {
-    if (window.utils.isDeactivateEvent(evt)) {
-      evt.stopPropagation();
+  /**
+   * [Вещаем обработчик события по клавиатуре]
+   * @param  {[object]} evt [передаем его в функцию показа формы загрузки]
+   */
+  var onShowImgLoaderOnkeydown = function (evt) {
+    if (window.utils.isActivateEvent(evt)) {
+      showImgLoader(evt);
     }
-  });
-};
+  };
 
-var onShowCroppFormClick = function () {
-  showCroppForm();
-};
+  /**
+   * [функция обработчик события для enter]
+   * @param  {[object]} evt [передаем его в функцию показа формы загрузки]
+   */
+  var onEnterKeydown = function (evt) {
+    if (window.utils.isDeactivateEvent(evt)) {
+      showImgLoader();
+    }
+  };
 
-/**
- * [Валидация поля хэш-тег]
- * @type {Number}
- */
-uploadHashrag.maxLength = 20;
+  /**
+   * [ показываем форму кадрирования & скрываем форму загрузки изображения]
+   * [Вешаем обработчик события на эскейп для закрытия окна]
+   * [отменяет закрытия окна по эскейп, когда фокус в комментарии]
+   */
+  var showCroppForm = function () {
+    uploadOverlay.classList.remove('hidden');
+    uploadImg.classList.add('hidden');
 
-/**
- * [Валидация поля для ввода комментариев]
- */
-commentArea.required = true;
-commentArea.minLength = 30;
-commentArea.maxLength = 100;
+    document.addEventListener('keydown', onEnterKeydown);
 
-/**
- * Обработчики событий
- */
-uploadCancel.addEventListener('click', onShowImgLoaderClick);
-uploadCancel.addEventListener('keydown', onShowImgLoaderOnkeydown);
-uploadFile.addEventListener('change', onShowCroppFormClick);
-showImgLoader();
-window.filters();
-window.controlScales(recizeControlFieldset, STEP_OF_ZOOM);
+    uploadComment.addEventListener('keydown', function (evt) {
+      if (window.utils.isDeactivateEvent(evt)) {
+        evt.stopPropagation();
+      }
+    });
+  };
+
+  var onShowCroppFormClick = function () {
+    showCroppForm();
+  };
+
+  /**
+   * [Валидация поля хэш-тег]
+   * @type {Number}
+   */
+  uploadHashrag.maxLength = 20;
+
+  /**
+   * [Валидация поля для ввода комментариев]
+   */
+  commentArea.required = true;
+  commentArea.minLength = 30;
+  commentArea.maxLength = 100;
+
+  /**
+   * Обработчики событий
+   */
+  uploadCancel.addEventListener('click', onShowImgLoaderClick);
+  uploadCancel.addEventListener('keydown', onShowImgLoaderOnkeydown);
+  uploadFile.addEventListener('change', onShowCroppFormClick);
+  showImgLoader();
+  window.filters();
+  window.controlScales(recizeControlFieldset, STEP_OF_ZOOM);
+
+})();
--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,161 +1,164 @@
 'use strict';
-
-var galleryOverlay = document.querySelector('.gallery-overlay');
-var galleryOverlayImage = document.querySelector('.gallery-overlay-image');
-var galleryClose = document.querySelector('.gallery-overlay-close');
-var likesCount = document.querySelector('.likes-count');
-var commentsCount = document.querySelector('.comments-count');
+(function () {
 
 
-var picturesContainer = document.querySelector('.pictures');
+  var galleryOverlay = document.querySelector('.gallery-overlay');
+  var galleryOverlayImage = document.querySelector('.gallery-overlay-image');
+  var galleryClose = document.querySelector('.gallery-overlay-close');
+  var likesCount = document.querySelector('.likes-count');
+  var commentsCount = document.querySelector('.comments-count');
 
-// функция обработчик события для enter
-var onEnterOnKeydown = function (evt) {
-  if (window.utils.isDeactivateEvent(evt)) {
-    closeGallery();
-  }
-};
 
-// открытия галереи
-var openGallery = function () {
-  galleryOverlay.classList.remove('hidden');
-  document.addEventListener('keydown', onEnterOnKeydown);
-};
+  var picturesContainer = document.querySelector('.pictures');
 
-// закрытия галерии
-var closeGallery = function () {
-  galleryOverlay.classList.add('hidden');
-};
-
-// вешаем обработчик события на закрытие по клику
-var onCloseGalleryClick = function () {
-  closeGallery();
-};
-
-// вешаем обработчик события на закрытие по нажатию на клавишу
-var onCloseGalleryOnKeydown = function (evt) {
-  if (window.utils.isActivateEvent(evt)) {
-    closeGallery();
-  }
-};
-
-/**
- * [получаем случайный элемент массива]
- * @return {[number]} [description]
- */
-var getRandomComment = function () {
-  var comments = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-
-  var randomElementIndex = Math.floor(Math.random() * comments.length);
-
-  return comments[randomElementIndex];
-};
-
-/**
- * [получаем рандомное целое число]
- * @param  {[number]} min [минимальное число]
- * @param  {[number]} max [Максимальное число]
- * @return {[number]}     [случайное целое число]
- */
-var getRandomInt = function (min, max) {
-  return Math.round(min + Math.random() * (max - min + 1));
-};
-
-/**
- * [собирает массив объектов]
- * @return {[object]} [возращает готовый объект с данными]
- */
-var getData = function () {
-  var pictures = [];
-
-  for (var i = 1; i <= 26; i++) {
-    var generateData = {
-      url: 'photos/' + i + '.jpg',
-      likes: getRandomInt(15, 200),
-      comments: getRandomComment()
-    };
-    pictures.push(generateData);
-  }
-  return pictures;
-};
-
-/**
- * [открывает галерею с заполнеными даными]
- * @param  {[evt]} evt  [evt объект]
- * @param  {[object]} data [объект с данными]
- */
-var showGallery = function (evt, data) {
-  evt.preventDefault();
-  renderGalleryImg(data);
-  openGallery(evt.target);
-};
-
-// открывает галерею по клику
-var onOpenPictureClick = function (imgDataObj) {
-  return function (evt) {
-    showGallery(evt, imgDataObj);
-  };
-};
-
-// открывает галерею по нажатию на энтер
-var onOpenPictureKeydown = function (imgDataObj) {
-  return function (evt) {
-    if (window.utils.isActivateEvent(evt)) {
-      showGallery(evt, imgDataObj);
+  // функция обработчик события для enter
+  var onEnterOnKeydown = function (evt) {
+    if (window.utils.isDeactivateEvent(evt)) {
+      closeGallery();
     }
   };
-};
 
-/**
- * [рендерит картинки]
- * @param  {[object]} dataObj [Объект с данными]
- * @return {[DOM]}  [Возрзщает дом элемент с заполнеными данными]
- */
-var renderImg = function (dataObj) {
-  var elementToClone = window.utils.getTemplateClone('#picture-template', '.picture');
-  var imageElement = elementToClone.cloneNode(true);
+  // открытия галереи
+  var openGallery = function () {
+    galleryOverlay.classList.remove('hidden');
+    document.addEventListener('keydown', onEnterOnKeydown);
+  };
 
-  imageElement.querySelector('img').src = dataObj.url;
-  imageElement.querySelector('.picture-likes').textContent = dataObj.likes;
-  imageElement.querySelector('.picture-comments').textContent = dataObj.comments.length;
+  // закрытия галерии
+  var closeGallery = function () {
+    galleryOverlay.classList.add('hidden');
+  };
 
-  imageElement.addEventListener('click', onOpenPictureClick(dataObj));
-  imageElement.addEventListener('keydown', onOpenPictureKeydown(dataObj));
+  // вешаем обработчик события на закрытие по клику
+  var onCloseGalleryClick = function () {
+    closeGallery();
+  };
 
-  return imageElement;
-};
+  // вешаем обработчик события на закрытие по нажатию на клавишу
+  var onCloseGalleryOnKeydown = function (evt) {
+    if (window.utils.isActivateEvent(evt)) {
+      closeGallery();
+    }
+  };
 
-/**
- * [рендерит картинку для галереи]
- * @param  {[object]} previewDataObj [объект с данными]
- */
-var renderGalleryImg = function (previewDataObj) {
-  galleryOverlayImage.src = previewDataObj.url;
-  likesCount.textContent = previewDataObj.likes;
-  commentsCount.textContent = previewDataObj.comments.length;
-};
+  /**
+   * [получаем случайный элемент массива]
+   * @return {[number]} [description]
+   */
+  var getRandomComment = function () {
+    var comments = [
+      'Всё отлично!',
+      'В целом всё неплохо. Но не всё.',
+      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+    ];
 
-// сохраняем в переменую массив объектов
-var arrMyData = getData();
+    var randomElementIndex = Math.floor(Math.random() * comments.length);
 
-/**
- * [добавляет в DOM картинки. Оптимизиует добавления путем использования fragment]
- */
-var appendNodes = function () {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i <= 25; i++) {
-    fragment.appendChild(renderImg(arrMyData[i]));
-  }
-  picturesContainer.appendChild(fragment);
-};
+    return comments[randomElementIndex];
+  };
 
-appendNodes();
-galleryClose.addEventListener('click', onCloseGalleryClick);
-galleryClose.addEventListener('keydown', onCloseGalleryOnKeydown);
+  /**
+   * [получаем рандомное целое число]
+   * @param  {[number]} min [минимальное число]
+   * @param  {[number]} max [Максимальное число]
+   * @return {[number]}     [случайное целое число]
+   */
+  var getRandomInt = function (min, max) {
+    return Math.round(min + Math.random() * (max - min + 1));
+  };
+
+  /**
+   * [собирает массив объектов]
+   * @return {[object]} [возращает готовый объект с данными]
+   */
+  var getData = function () {
+    var pictures = [];
+
+    for (var i = 1; i <= 26; i++) {
+      var generateData = {
+        url: 'photos/' + i + '.jpg',
+        likes: getRandomInt(15, 200),
+        comments: getRandomComment()
+      };
+      pictures.push(generateData);
+    }
+    return pictures;
+  };
+
+  /**
+   * [открывает галерею с заполнеными даными]
+   * @param  {[evt]} evt  [evt объект]
+   * @param  {[object]} data [объект с данными]
+   */
+  var showGallery = function (evt, data) {
+    evt.preventDefault();
+    renderGalleryImg(data);
+    openGallery(evt.target);
+  };
+
+  // открывает галерею по клику
+  var onOpenPictureClick = function (imgDataObj) {
+    return function (evt) {
+      showGallery(evt, imgDataObj);
+    };
+  };
+
+  // открывает галерею по нажатию на энтер
+  var onOpenPictureKeydown = function (imgDataObj) {
+    return function (evt) {
+      if (window.utils.isActivateEvent(evt)) {
+        showGallery(evt, imgDataObj);
+      }
+    };
+  };
+
+  /**
+   * [рендерит картинки]
+   * @param  {[object]} dataObj [Объект с данными]
+   * @return {[DOM]}  [Возрзщает дом элемент с заполнеными данными]
+   */
+  var renderImg = function (dataObj) {
+    var elementToClone = window.utils.getTemplateClone('#picture-template', '.picture');
+    var imageElement = elementToClone.cloneNode(true);
+
+    imageElement.querySelector('img').src = dataObj.url;
+    imageElement.querySelector('.picture-likes').textContent = dataObj.likes;
+    imageElement.querySelector('.picture-comments').textContent = dataObj.comments.length;
+
+    imageElement.addEventListener('click', onOpenPictureClick(dataObj));
+    imageElement.addEventListener('keydown', onOpenPictureKeydown(dataObj));
+
+    return imageElement;
+  };
+
+  /**
+   * [рендерит картинку для галереи]
+   * @param  {[object]} previewDataObj [объект с данными]
+   */
+  var renderGalleryImg = function (previewDataObj) {
+    galleryOverlayImage.src = previewDataObj.url;
+    likesCount.textContent = previewDataObj.likes;
+    commentsCount.textContent = previewDataObj.comments.length;
+  };
+
+  // сохраняем в переменую массив объектов
+  var arrMyData = getData();
+
+  /**
+   * [добавляет в DOM картинки. Оптимизиует добавления путем использования fragment]
+   */
+  var appendNodes = function () {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i <= 25; i++) {
+      fragment.appendChild(renderImg(arrMyData[i]));
+    }
+    picturesContainer.appendChild(fragment);
+  };
+
+  appendNodes();
+  galleryClose.addEventListener('click', onCloseGalleryClick);
+  galleryClose.addEventListener('keydown', onCloseGalleryOnKeydown);
+})();
